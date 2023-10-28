a6b7a3ec8ee8647f36db03ee352c108edfd699e4
Ishaaq <ishaaqziyan@gmail.com>
2023-10-28 11:37:19 +0530

    Deployed User Contexts and Sign Out Functionality

--- ebase/src/routes/navigation/navigation.component.jsx
+++ ebase/src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,21 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import './navigation.styles.scss';
+import { UserContext } from "../../contexts/user.context";
+import {signOutUser} from "../../utils/firebase/firebase.utils"
 
 const Navigation = () => {
+  const {currentUser, setCurrentUser} = useContext(UserContext);
+  //console.log(currentUser);
+
+  const signOutHandler = async () =>{
+    await signOutUser();
+    setCurrentUser(null);
+    
+  }
+
     return(
      <Fragment>
             <div className='navigation'>
@@ -15,9 +26,13 @@ const Navigation = () => {
                <Link className="nav-link" to='/shop'>
                    SHOP
                </Link>
-               <Link className="nav-link" to='/auth'>
-                   SIGN IN
-               </Link>
+               {
+                currentUser ? (
+                  <span className="nav-link" onClick={signOutHandler}>SIGN OUT</span>)
+                  :( <Link className="nav-link" to='/auth'>
+                  SIGN IN
+              </Link>)
+               }
              </div>
             </div>
             <Outlet/>
