2edfb9d129b92eead1add47969ad08ade68c0ed3
Ishaaq <ishaaqziyan@gmail.com>
2023-11-01 16:50:10 +0530

    Created Checkout Page and i's Functionality

--- ebase/src/contexts/cart.context.jsx
+++ ebase/src/contexts/cart.context.jsx
@@ -14,11 +14,28 @@ const addCartItem = (cartItems, productToAdd) =>{
    return [...cartItems, {...productToAdd, quantity:1}];
 }
 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+ //find the cart item to remove
+ const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartItemToRemove.id);
+
+ //check if quantity is equal to 1 if so remove it
+if(existingCartItem.quantity === 1){
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+}
+
+ //else return cartitems with reduced quantity
+ return cartItems.map((cartItem) => cartItem.id === cartItemToRemove.id ?
+ {...cartItem, quantity: cartItem.quantity -1}
+ : cartItem
+ );
+};
+
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
     cartItems: [],
     addItemToCart:() =>{},
+    removeItemFromCart : () => {},
     cartCount:0
 });
 
@@ -35,7 +52,11 @@ export const CartProvider = ({children}) => {
     const addItemToCart = (productToAdd) => {
      setCartItems(addCartItem(cartItems,productToAdd))
     }
-    const value = {isCartOpen, setIsCartOpen,addItemToCart, cartItems, cartCount};
+    const removeItemFromCart = (cartItemToRemove) => {
+        setCartItems(removeCartItem(cartItems,cartItemToRemove));
+    }
+
+    const value = {isCartOpen, setIsCartOpen,addItemToCart, removeItemFromCart ,cartItems, cartCount};
     return(
         <CartContext.Provider value={value}>{children}</CartContext.Provider>
     )
---
+++ ebase/src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,28 @@
+import './checkout.styles.scss';
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
+
+
+const Checkout = () => {
+    const {cartItems, addItemToCart,removeItemFromCart} = useContext(CartContext);
+    return(
+        <div>
+            <h1>I am the checkout page</h1>
+            <div>
+                {cartItems.map((cartItem) => {
+                    const {id, name, quantity} = cartItem;
+                     return <div key ={id}>
+                        <h2>{name}</h2>
+                        <span>{quantity}</span>
+                        <br/>
+                        <span onClick={() => removeItemFromCart(cartItem)}>decrement</span>
+                        <br/>
+                        <span onClick ={()=> addItemToCart(cartItem)}>increment</span>
+                        <br/>
+                     </div> 
+                     })}
+            </div>
+        </div>
+    )
+}
+export default Checkout;
