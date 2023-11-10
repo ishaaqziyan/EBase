9aecd41e359c43c48e682c07eadcce29bc48f138
Ishaaq <ishaaqziyan@gmail.com>
2023-11-10 17:41:53 +0530

    Deploying Redux

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,16 @@
 import { Fragment, useContext } from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet} from "react-router-dom";
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { NavigationContainer, NavLinks, NavLink, LogoContainer } from "./navigation.styles";
-import { UserContext } from "../../contexts/user.context";
 import {signOutUser} from "../../utils/firebase/firebase.utils";
 import CartDropDown from '../../components/cart-dropdown/cart-dropdown-component';
-import { CartContext, CartProvider } from "../../contexts/cart.context";
+import { CartContext} from "../../contexts/cart.context";
+import { useSelector } from "react-redux"; 
+import { selectCurrentUser } from "../../store/user/user.selector";
 
 const Navigation = () => {
-  const {currentUser} = useContext(UserContext);
+ const currentUser = useSelector(selectCurrentUser);
   const {isCartOpen} = useContext(CartContext);
 
     return(
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,12 +3,13 @@ import CategoriesPreview from '../categories-preiew/categories-preview.component
 import Category from '../category/category.component';
 import './shop.styles.scss';
 
+
 const Shop = () => {
   return (
-    <Routes> 
-        <Route index element={<CategoriesPreview/>} />
-        <Route path=':category' element={<Category/>} />
-    </Routes>
+      <Routes> 
+          <Route index element={<CategoriesPreview/>} />
+          <Route path=':category' element={<Category/>} />
+      </Routes>
   );
 };
 
