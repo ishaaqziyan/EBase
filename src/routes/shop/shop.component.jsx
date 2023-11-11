6f4cc977ccbf5da3a6ff4a996ff76993a84a6c46
Ishaaq <ishaaqziyan@gmail.com>
2023-11-11 15:55:13 +0530

    Deploying Reducers, Selectors and Middleware

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,15 +1,31 @@
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+
+import { setCategories } from '../../store/categories/category.action';
+
 import CategoriesPreview from '../categories-preiew/categories-preview.component';
 import Category from '../category/category.component';
-import './shop.styles.scss';
-
 
 const Shop = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoriesArray = await getCategoriesAndDocuments('categories');
+      console.log(categoriesArray);
+      dispatch(setCategories(categoriesArray));
+    };
+
+    getCategoriesMap();
+  }, []);
+
   return (
-      <Routes> 
-          <Route index element={<CategoriesPreview/>} />
-          <Route path=':category' element={<Category/>} />
-      </Routes>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
+    </Routes>
   );
 };
 
