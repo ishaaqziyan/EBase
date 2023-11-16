6680e282213a4f473e92b44fdf0736ee6a0572b4
Ishaaq <ishaaqziyan@gmail.com>
2023-11-16 15:58:17 +0530

    Deploying Redux-Saga and Initiated Migration

--- src/store/store.js
+++ src/store/store.js
@@ -3,8 +3,9 @@ import {persistStore, persistReducer} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
-import thunk from 'redux-thunk';
-
+// import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import { rootSaga } from './root-saga';
 
 const persistConfig = {
   key: 'root',
@@ -12,14 +13,17 @@ const persistConfig = {
   whitelist: ['cart'],
 }
 
+const sagaMiddleware = createSagaMiddleware();
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean); 
+const middleWares = [process.env.NODE_ENV !== 'production' && logger,sagaMiddleware].filter(Boolean); 
 
 const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined,composedEnhancers);
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
